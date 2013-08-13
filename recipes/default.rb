768816cde0f3d7fea5adac546a0a14689340796b
Ranjib Dey <ranjib@pagerduty.com>
2013-08-12 23:34:32 -0700

    unit testing is important

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,5 @@
 
-extend Etcd::RecipeHelper
+extend Etcd::Recipe::Helper
 
 Chef::Log.info("You are usng #{Etcd::RecipeHelper::VERSION}")
 Chef::Log.info("'One of these days, Im going to cut you in little pieces'- PinkFloyd")
